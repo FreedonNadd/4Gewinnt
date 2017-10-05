5d932b9ffecf61038930bd7910eb2f69f04c9bfc
FreedonNadd <freedonnadd@everpod.de>
2017-10-05 09:26:41 +0200

    Zwischenstand

---
+++ 4GewinntLib/Classes/Place.cs
@@ -0,0 +1,52 @@
+﻿using _4GewinntLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4GewinntLib.Classes
+{
+    public class Place : IPlace
+    {
+        int _x;
+        int _y;
+        IStone _content;
+
+        public Place(int X, int Y)
+        {
+            _x = X;
+            _y = Y;
+        }
+
+        public IStone Content
+        {
+            get
+            {
+                return _content;
+            }
+
+            set
+            {
+                _content = value;
+            }
+        }
+
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+        }
+        
+        public int Y
+        {
+            get
+            {
+                return _y;
+            }
+        }
+        
+    }
+}
---
+++ 4GewinntLib/Interfaces/IBoard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4GewinntLib.Interfaces
+{
+    public interface IBoard
+    {
+        bool PlaceStone(int Y);
+
+        IPlayer EvaluateWinner();
+
+        List<List<IPlace>> PlayField { get; }
+
+        IPlayer Winner { get; }
+    }
+}
